9b5868fac14dd206cbfa10606035d6a0082c4b40
vsevolod <mirpormetro@yandex.ru>
2024-07-30 22:16:42 +0400

    1

---
+++ Zadaniye 3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3
+// void Main()
+// {
+//     int[] num = new int[] { 1, 2, 5, 0, 10, 34 };
+//     Print2(num);
+// }
+// Main();
+//   static void Print2(int[] num)
+//         {
+//             if (num.Length == 0) return;
+//             else
+//             {
+//                 Print2(num.Skip(1).ToArray());
+//                 Console.Write(" " + num[0]);
+//             }
+//         }
